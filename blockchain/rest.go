bd5f797f41b8b3f12634411606f31a4e298539b4
Solovyov1796 <xinyu@0g.ai>
2025-01-08 17:50:24 +0800

    add debug log

--- blockchain/rest.go
+++ blockchain/rest.go
@@ -1,7 +1,11 @@
 package blockchain
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/go-resty/resty/v2"
+	"github.com/sirupsen/logrus"
 )
 
 func IsValidatorJailed(url string) (bool, error) {
@@ -14,6 +18,13 @@ func IsValidatorJailed(url string) (bool, error) {
 
 	if resp.StatusCode() != 200 {
 		return false, ErrorNotSuccess
+	}
+
+	if logrus.IsLevelEnabled(logrus.DebugLevel) {
+		jsonStr, _ := json.Marshal(result)
+		logrus.WithFields(logrus.Fields{
+			"response": fmt.Sprintf("%+v", string(jsonStr)),
+		}).Debug("response of cometbft rpc: validators?height=")
 	}
 
 	return result["validator"].(map[string]interface{})["jailed"].(bool), nil
